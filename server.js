06762a026f47a23104e916cd61747ab5367aca39
Joshua Maney <joshuaraymaney@gmail.com>
2021-04-11 16:42:12 -0400

    added routes and functions to write and save notes

--- server.js
+++ server.js
@@ -1,93 +1,51 @@
+// required dependencies
 const express = require('express');
 const fs = require('fs');
 var app = express();
 var path = require('path');
 
+// establish a port to be used
 const PORT = 8081;
 
+//serves files in a public directory
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-app.get("/", function(req, res) {
-    res.json(path.join(__dirname, "public/index.html"));
-  });
+// routes
+app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
 
-  app.get("/", function(req, res) {
-    res.json(path.join(__dirname, "public/notes.html"));
-  });
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "/public/index.html"));
+});
 
-  app.post("/api/notes", function (req, res) {
-    fs.readFile(__dirname + "/db/db.json", 'utf8', function (error, notes) {
-      if (error) {
-        return console.log(error)
-      }
-      notes = JSON.parse(notes)
-  
-      let id = notes[notes.length - 1].id + 1
-      let newNote = { title: req.body.title, text: req.body.text, id: id }
-      let activeNote = notes.concat(newNote)
-  
-      fs.writeFile(__dirname + "/db/db.json", JSON.stringify(activeNote), function (error, data) {
-        if (error) {
-          return error
-        }
-        console.log(activeNote)
-        res.json(activeNote);
-      })
-    })
-  })
-  
-  // Pull from db.json
-  app.get("/api/notes", function (req, res) {
-    fs.readFile(__dirname + "/db/db.json", 'utf8', function (error, data) {
-      if (error) {
-        return console.log(error)
-      }
-      console.log("Notes", data)
-      res.json(JSON.parse(data))
-    })
-  });
-  
-  app.delete("/api/notes/:id", function (req, res) {
-    const noteId = JSON.parse(req.params.id)
-    console.log(noteId)
-    fs.readFile(__dirname + "/db/db.json", 'utf8', function (error, notes) {
-      if (error) {
-        return console.log(error)
-      }
-      notes = JSON.parse(notes)
-  
-      notes = notes.filter(val => val.id !== noteId)
-  
-      fs.writeFile(__dirname + "/db/db.json", JSON.stringify(notes), function (error, data) {
-        if (error) {
-          return error
-        }
-        res.json(notes)
-      })
-    })
-  })
-  
-  app.put("/api/notes/:id", function(req, res) {
-    const noteId = JSON.parse(req.params.id)
-    console.log(noteId)
-    fs.readFile(__dirname + "db/db.json", "utf8", function(error, notes) {
-      if (error ){
-        return console.log(error)
-      }
-      notes.JSONparse(notes)
-  
-      notes = notes.filter(val => val.id !== noteId)
-  
-      fs.writeFile(__dirname +"db/db.json", JSON.stringify(notes), function (error, data) {
-        if (error) {
-          return error
-        }
-        res.json(notes)
-      })
-    })
-  })
+app.get("/api/notes", (req, res) => {
+    readFileAsync("./db/db.json", "utf8")
+    .then((res, err) => {
+        if(err) console.log(err);
+        return res.json(JSON.parse(result));
+    });
+});
+
+app.post("/api/notes", (req, res) => {
+    let newNote = req.body;
+    readFileAsync("./db/db.json", "utf8")
+    .then((result, err) => {
+        if(err) console.log(err);
+        return Promise.resolve(JSON.parse(result));
+    }).then(data => {
+        newNote.id = getLastIndex(data) + 1;
+        (data.length > 0)? data.push(newNote):data = [newNote];
+        return Promise.resolve(data);
+    }).then(data => {
+        writeFileAsync("./db/db.json", JSON.stringify(data));
+        res.json(newNote);
+    }).catch(err => {
+        if(err) throw err;
+    });
+});
 
 app.listen(PORT, () => {
     // Log (server-side) when our server has started
