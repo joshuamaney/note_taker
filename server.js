1d1f24c702028dafc32cc2584d6eb75abc9733c1
Joshua Maney <joshuaraymaney@gmail.com>
2021-04-11 17:14:19 -0400

    fixed a directory issue

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const fs = require('fs');
 const app = express();
 const path = require('path');
-let note = require("/db/db.json");
+let note = require("./db/db.json");
 
 // establish a port to be used
 const PORT = 8081;
@@ -27,24 +27,6 @@ app.get("/api/notes", (req, res) => {
     .then((res, err) => {
         if(err) console.log(err);
         return res.json(JSON.parse(result));
-    });
-});
-
-app.post("/notes", (req, res) => {
-    let newNote = req.body;
-    readFile("/db/db.json", "utf8")
-    .then((result, err) => {
-        if(err) console.log(err);
-        return Promise.resolve(JSON.parse(result));
-    }).then(data => {
-        newNote.id = getLastIndex(data) + 1;
-        (data.length > 0)? data.push(newNote):data = [newNote];
-        return Promise.resolve(data);
-    }).then(data => {
-        writeFile("/db/db.json", JSON.stringify(data));
-        res.json(newNote);
-    }).catch(err => {
-        if(err) throw err;
     });
 });
 
