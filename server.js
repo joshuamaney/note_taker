a0b23fb0331cb5908cd14fd19ec874121d5019e7
Joshua Maney <joshuaraymaney@gmail.com>
2021-04-12 13:08:22 -0400

    still working on the exports, but getting closer

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const fs = require('fs');
 const app = express();
 const path = require('path');
-const database = require('./db/db.json');
+const db = require('./db/db.json');
 
 // establish a port to be used
 const PORT = 8081;
@@ -15,24 +15,43 @@ app.use(express.static("public"));
 
 // routing
 
-// module.exports = (app) => {
-
     app.get("/notes", (req, res) => {
         res.sendFile(path.join(__dirname, "./public/notes.html"));
     });
 
-    app.get("*", (req, res) => {
+    app.get("/", (req, res) => {
         res.sendFile(path.join(__dirname, "./public/index.html"));
-    });
+    });    
 
+module.exports = (app) => {
+    
     app.get("/api/notes", (req, res) => {
-        readFile("./db/db.json", "utf8")
-        .then((res, err) => {
-            if(err) console.log(err);
-            return res.json(JSON.parse(result));
-        });
+        res.json(db);
+      });
+
+    app.post("/api/notes", function (req, res) {
+        const newNote = {
+          id: db.length + 1,
+          title: req.body.title,
+          text: req.body.text
+        };
+        db.push(newNote);
+        res.json(db);
+      });
+
+    app.delete("/api/notes/:id", function (req, res) {
+        const reqId = req.params.id;
+        let note = db.filter(note => {
+          return note.id == reqId;
+        })[0];
+        const index = db.indexOf(note);
+        db.splice(index, 1);
+        for (var i = index; i < db.length; i++) {
+          db[i].id--;
+    }
+        res.json(db);
     });
-// };
+};
 
 app.listen(PORT, () => {
     // Log (server-side) when our server has started
