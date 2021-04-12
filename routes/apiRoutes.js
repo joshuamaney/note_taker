c85ef8807ed4df68a0114134b9bd0e49de5ee4a5
Joshua Maney <joshuaraymaney@gmail.com>
2021-04-12 18:15:05 -0400

    fixed the app.post

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -4,20 +4,9 @@ const fs = require('fs');
 module.exports = (app) => {
     app.get('/api/notes', (req, res) => res.json(db));
 
-    app.post("/api/notes", (req, res) => {
-        let noteData =fs.readFileSync(db, 'utf8');
-            console.log(noteData);
-        noteData = JSON.parse(noteData);
-        let noteLength = noteData.length;
-    
-        req.body.id = noteLength;
-        note.push(req.body);
-        noteData = JSON.stringify(noteData);
-    
-        fs.writeFile(db, noteData, 'utf8', (err) => {
-            if (err) throw err;
-        });
-        res.json(JSON.parse(noteData));
-    });
+    app.post('/api/notes', (req, res) => {
+        db.push(req.body);
+        res.json("saved");
+      });
     
 };
