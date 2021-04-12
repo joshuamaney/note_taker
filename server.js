44f43f1de280107a3741317e9434fdd123f68332
Joshua Maney <joshuaraymaney@gmail.com>
2021-04-12 12:42:01 -0400

    working on the routing

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const express = require('express');
 const fs = require('fs');
 const app = express();
 const path = require('path');
-let note = require("./db/db.json");
 
 // establish a port to be used
 const PORT = 8081;
@@ -13,22 +12,26 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-// routes
-app.get("/notes", (req, res) => {
-    res.sendFile(path.join(__dirname, "public/notes.html"));
-});
+// routing
 
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "public/index.html"));
-});
+module.exports = (app) => {
 
-app.get("/api/notes", (req, res) => {
-    readFile("/db/db.json", "utf8")
-    .then((res, err) => {
-        if(err) console.log(err);
-        return res.json(JSON.parse(result));
+    app.get("/notes", (req, res) => {
+        res.sendFile(path.join(__dirname, "public/notes.html"));
     });
-});
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "public/index.html"));
+    });
+
+    app.get("/api/notes", (req, res) => {
+        readFile("/db/db.json", "utf8")
+        .then((res, err) => {
+            if(err) console.log(err);
+            return res.json(JSON.parse(result));
+        });
+    });
+};
 
 app.listen(PORT, () => {
     // Log (server-side) when our server has started
