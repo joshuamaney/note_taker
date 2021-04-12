ff1bbaa10c9995cb519691d4e0ecb91cefa1b467
Joshua Maney <joshuaraymaney@gmail.com>
2021-04-12 16:35:55 -0400

    still working out the app.post, just saving for reference

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,7 +1,23 @@
-const path = require('path');
-const db = require('../db/db.json');
+const db = require('../db/db.json')
 const fs = require('fs');
 
 module.exports = (app) => {
     app.get('/api/notes', (req, res) => res.json(db));
+
+    app.post("/api/notes", (req, res) => {
+        let noteData =fs.readFileSync(db, 'utf8');
+            console.log(noteData);
+        noteData = JSON.parse(noteData);
+        let noteLength = noteData.length;
+    
+        req.body.id = noteLength;
+        note.push(req.body);
+        noteData = JSON.stringify(noteData);
+    
+        fs.writeFile(db, noteData, 'utf8', (err) => {
+            if (err) throw err;
+        });
+        res.json(JSON.parse(noteData));
+    });
+    
 };
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,11 +3,11 @@ const path = require('path');
 module.exports = (app) => {
     // to notes
     app.get("/notes", function(req, res) {
-      res.sendFile(path.join(__dirname, "../public/notes.html"));
+      res.sendFile(path.join(__dirname, '../public/notes.html'));
     });
   
     // If no matching route is found default to home
     app.get("*", (req, res) => {
-      res.sendFile(path.join(__dirname, "../public/index.html"));
+      res.sendFile(path.join(__dirname, '../public/index.html'));
     });
 };
