6a3e57797c53a6f7e3b4169d630054ef0217a8d9
Joshua Maney <joshuaraymaney@gmail.com>
2021-04-13 18:49:33 -0400

    fixed the app.post and app.delete functions. Everything seems to work

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,12 +1,46 @@
-const db = require('../db/db.json')
 const fs = require('fs');
+let noteData = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
 
 module.exports = (app) => {
-    app.get('/api/notes', (req, res) => res.json(db));
+ // GET notes
+    app.get("/api/notes", (req, res) => {
+        res.json(noteData);
+    });
 
-    app.post('/api/notes', (req, res) => {
-        db.push(req.body);
-        res.json('saved');
-      });
-    
-};
+    app.get("/api/notes/:id", (req, res) => {
+        res.json(noteData[Number(req.params.id)]);
+    });
+
+// POST notes
+    app.post("/api/notes", (req, res) => {
+
+        let newNote = req.body;
+        let noteId = (noteData.length).toString();
+        console.log(`Note id`, noteId, `created`);
+        newNote.id = noteId;
+        noteData.push(newNote);
+        
+        fs.writeFileSync("./db/db.json", JSON.stringify(noteData), (err) => {
+            if (err) throw (err);        
+        }); 
+
+        res.json(noteData);    
+    });
+
+    // DELETE notes
+    app.delete("/api/notes/:id", (req, res) => {
+
+        let noteId = req.params.id;
+        let newId = 0;
+        console.log(`Deleted note ${noteId}`);
+        noteData = noteData.filter(currNote => {
+           return currNote.id != noteId;
+        });
+        for (currNote of noteData) {
+            currNote.id = newId.toString();
+            newId++;
+        }
+        fs.writeFileSync("./db/db.json", JSON.stringify(noteData));
+        res.json(noteData);
+    }); 
+}
