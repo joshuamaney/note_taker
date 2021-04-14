6cc1e7838455831bcd18528ae6a1afe36aef5830
Joshua Maney <joshuaraymaney@gmail.com>
2021-04-13 22:06:31 -0400

    fixed indentation issues in api routes and deleted old, duplicate GET function

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,21 +2,18 @@ const fs = require('fs');
 let noteData = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
 
 module.exports = (app) => {
- // GET notes
+ // GET notes from db
     app.get("/api/notes", (req, res) => {
         res.json(noteData);
     });
 
-    app.get("/api/notes/:id", (req, res) => {
-        res.json(noteData[Number(req.params.id)]);
-    });
-
-// POST notes
+// POST notes and save to db
     app.post("/api/notes", (req, res) => {
 
         let newNote = req.body;
         let noteId = (noteData.length).toString();
-        console.log(`Note id`, noteId, `created`);
+            console.log(`Note id`, noteId, `created`);
+
         newNote.id = noteId;
         noteData.push(newNote);
         
@@ -32,7 +29,8 @@ module.exports = (app) => {
 
         let noteId = req.params.id;
         let newId = 0;
-        console.log(`Deleted note ${noteId}`);
+            console.log(`Deleted note ${noteId}`);
+
         noteData = noteData.filter(currNote => {
            return currNote.id != noteId;
         });
